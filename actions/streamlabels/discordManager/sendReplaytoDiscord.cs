38664fb478bae354606af29b9767a0f22a949acc
BlackyWerSonst <t.vogt@avsgmbh.de>
2022-07-11 14:08:35 +0200

    Init Early Discord Action ideas

---
+++ actions/streamlabels/discordManager/sendReplaytoDiscord.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+public class CPHInline
+{
+
+    public bool Execute()
+    {
+        var discordWebhook = args["webhook"].ToString();
+        var user = args["userName"];
+        var title = args["rawInput"];
+
+        CPH.LogInfo($"{user} is creating a clip....");
+
+        // Checking if stream is Live
+        if (CPH.ObsIsStreaming()){
+            // Use replay Buffer to create a clip with better Quality
+            var clip = CPH.ObsReplayBufferSave();
+
+
+
+            // Post clip Link in Chat
+            CPH.SendMessage($"imGlitch @{user} clipped {clip.Url}");
+
+            // Post to Discord Webhook
+            CPH.LogInfo($"Posting clip to discord....");
+
+            string discordJson = JsonConvert.SerializeObject(new
+            {
+                content = $"{user} just clipped {title}\n{clip.Url}"
+            });
+
+            var body = new StringContent(
+                discordJson,
+                System.Text.Encoding.UTF8,
+                "application/json"
+                );
+
+            HttpClient httpClient = new HttpClient();
+            try
+            {
+                httpClient.PostAsync(discordWebhook, body).Wait();
+                httpClient.Dispose();
+
+            }
+            catch (Exception e) {
+                CPH.LogInfo("Could't perfom post to discord webhook");
+            }
+
+        }
+        else
+        {
+            CPH.SendMessage($"imGlitch @{user}, clips cant be made when the stream is offline");
+        }
+
+        return true;
+    }
+}

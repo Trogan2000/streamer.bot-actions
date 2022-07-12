a4e955aa8f221e58b002c0994fa1607063bccb5a
BlackyWerSonst <t.vogt@avsgmbh.de>
2022-07-12 10:29:57 +0200

    Idea Update
    Update

--- actions/discordManager/sendReplaytoDiscord.cs
+++ actions/discordManager/sendReplaytoDiscord.cs
@@ -1,58 +1,54 @@
 using System;
-using System.Net.Http;
-using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+using System.Linq;
 
 public class CPHInline
 {
 
+    /* Step 1
+     *  Create a ReplayBuffer Clip
+     * Step 2 
+     *  Save it on PC
+     * Step 3 
+     *  Send ReplayBuffer Clip to Discord Channel
+     */
     public bool Execute()
     {
-        var discordWebhook = args["webhook"].ToString();
-        var user = args["userName"];
-        var title = args["rawInput"];
 
-        CPH.LogInfo($"{user} is creating a clip....");
+        string yourReplayPath = "D:\\Videos\\";
+        string yourOutputPath = "D:\\Videos\\Clips\\";
 
-        // Checking if stream is Live
-        if (CPH.ObsIsStreaming()){
-            // Use replay Buffer to create a clip with better Quality
-            var clip = CPH.ObsReplayBufferSave();
+        string discordWebhook = args["webhook"].ToString();
+        string user = args["userName"].ToString();
+        string title = args["rawInput"].ToString();
 
-
-
-            // Post clip Link in Chat
-            CPH.SendMessage($"imGlitch @{user} clipped {clip.Url}");
-
-            // Post to Discord Webhook
-            CPH.LogInfo($"Posting clip to discord....");
-
-            string discordJson = JsonConvert.SerializeObject(new
-            {
-                content = $"{user} just clipped {title}\n{clip.Url}"
-            });
-
-            var body = new StringContent(
-                discordJson,
-                System.Text.Encoding.UTF8,
-                "application/json"
-                );
-
-            HttpClient httpClient = new HttpClient();
-            try
-            {
-                httpClient.PostAsync(discordWebhook, body).Wait();
-                httpClient.Dispose();
-
-            }
-            catch (Exception e) {
-                CPH.LogInfo("Could't perfom post to discord webhook");
-            }
-
-        }
-        else
+        // If there's no input from the chatter, it won't save the clip.
+        if (String.IsNullOrEmpty(title))
         {
-            CPH.SendMessage($"imGlitch @{user}, clips cant be made when the stream is offline");
+            var errMsg = user + ", you must specify a name for the clip.";
+            CPH.SendMessage(errMsg);
+            return true;
         }
+
+        CPH.ObsReplayBufferSave();
+        System.Threading.Thread.Sleep(2000);
+        // Scan the initial folder where the clip is saved for the newest file created - presumably the clip
+        var clipBackups = new DirectoryInfo(@"" + yourReplayPath + "");
+        // Filter by MKV files - or change to whatever file output format you have your replay buffer configured to.
+        var clipFile = clipBackups.GetFiles("*.mkv").OrderByDescending(p => p.CreationTime).FirstOrDefault();
+        // If no file found, quit out.
+        if (clipFile == null){
+            var errMsg = args["userName"].ToString() + ", the clip couldn't be saved.";
+            CPH.SendMessage(errMsg);
+            return true;
+        }
+
+        // Grab the file name by the Windows Path
+        var clipPath = clipFile.FullName;
+        // Rename the file
+        string newFileName = @"" + yourOutputPath + title + " (clipped by " + user + ").mkv";
+        System.IO.File.Move(clipPath, newFileName);
 
         return true;
     }
